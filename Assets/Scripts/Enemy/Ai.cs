569b139fd2c47864ef17846691fb4bd8853a1ce3
LowArte <47986420+LowArte@users.noreply.github.com>
2019-03-06 16:20:33 +0300

    asdasdas

--- Assets/Scripts/Enemy/Ai.cs
+++ Assets/Scripts/Enemy/Ai.cs
@@ -8,25 +8,22 @@ using System.Linq;
 public class Ai : NetworkBehaviour
 {
     public float dist;
-    
+
     NavMeshAgent nav;
     public float AgrRadius = 20f;
     public float atdist = 2f;
     Transform target;
-    Animator anim;
-    // Start is called before the first frame update
+    NetworkAnimator anim;
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-        
-        anim = GetComponent<Animator>();
-  
+        anim = GetComponent<NetworkAnimator>();
     }
 
     private void FindPlayer()
     {
         int index = 0;
-        var players = GameObject.FindGameObjectsWithTag("Player").ToList().FindAll(x=> x.GetComponent<PlayerController>().IsDead == false).ToArray();
+        var players = GameObject.FindGameObjectsWithTag("Player").ToList().FindAll(x => x.GetComponent<PlayerController>().IsDead == false).ToArray();
         if (players.Length == 0)
             return;
         float minDis = Vector3.Distance(this.transform.position, players[0].transform.position);
@@ -41,7 +38,6 @@ public class Ai : NetworkBehaviour
         }
         target = players[index].transform;
     }
-    // Update is called once per frame
     void Move()
     {
         if (target == null)
@@ -49,40 +45,41 @@ public class Ai : NetworkBehaviour
         dist = Vector3.Distance(target.transform.position, transform.position);
         if (dist > AgrRadius)
         {
-            anim.SetBool("idle",true);
+            anim.animator.SetBool("idle", true);
         }
 
         if ((dist < AgrRadius) && (dist > atdist))
         {
             nav.SetDestination(target.position);
-            anim.SetBool("idle", false);
+            anim.animator.SetBool("idle", false);
             anim.SetTrigger("run");
         }
 
         if (dist < atdist)
         {
-            anim.SetBool("idle", false);
+            anim.animator.SetBool("idle", false);
             anim.SetTrigger("attack");
         }
     }
 
     void Update()
     {
-        if(target !=null)
+        if (target != null)
         {
             if (target.GetComponent<PlayerController>().IsDead)
             {
                 target = null;
-                anim.SetBool("idle",true);
+                anim.animator.SetBool("idle", true);
                 dist = int.MaxValue;
                 FindPlayer();
             }
-
         }
         else
         {
             FindPlayer();
         }
-        Move();   
+        Move();
     }
 }
+
+
